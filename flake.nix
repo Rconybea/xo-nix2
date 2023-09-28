20aa1cd143b6387d47c66246bdd69fe594c6390a
Roland Conybeare <rconybeare@gmail.com>
2023-09-28 00:35:31 -0400

    experiment

--- flake.nix
+++ flake.nix
@@ -3,29 +3,45 @@
   description = "A very basic flake";
 
   # dependencies of this flake
-  inputs = {
+  inputs = rec {
     #nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/5d017a8822e0907fb96f7700a319f9fe2434de02.tar.gz";
     nixpkgs.url = "github:nixos/nixpkgs/23.05";
+
+    xo_cmake = {
+      url = "path:flakes/xo-cmake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  # function inputs -> set
-  # structure of set should be like:
-  #   packages|devShells|checks ..
-  #     targetarchitecture
-  #       outputname
-  # note however that templates,overlays omit the targetarchitecture layer
-  #
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, xo_cmake } :
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in {
       packages.${system} = rec {
-        # NOTE: ./pkgs tree has to be committed to git for any of the below to work!
-
-        xo_cmake = pkgs.callPackage ./pkgs/xo-cmake {};
-        #hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-        #default = self.packages.x86_64-linux.hello;
       };
     };
+
+#  # function inputs -> set
+#  # structure of set should be like:
+#  #   packages|devShells|checks ..
+#  #     targetarchitecture
+#  #       outputname
+#  # note however that templates,overlays omit the targetarchitecture layer
+#  #
+#  outputs = { self, nixpkgs }:
+#    let
+#      system = "x86_64-linux";
+#      pkgs = import nixpkgs { inherit system; };
+#    in {
+#      packages.${system} = rec {
+#        # NOTE: ./pkgs tree has to be committed to git for any of the below to work!
+#
+#        # pulling in a separate flake,  but replacing one of its inputs
+#
+#        xo_cmake = pkgs.callPackage ./pkgs/xo-cmake {};
+#        #hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+#        #default = self.packages.x86_64-linux.hello;
+#      };
+#    };
 }
