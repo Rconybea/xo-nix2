2f897b78c2087a4932bb1d354152ada5bdd7746f
Roland Conybeare <rconybeare@gmail.com>
2023-09-28 16:16:25 -0400

    squash build

--- flake.nix
+++ flake.nix
@@ -12,14 +12,24 @@
 #      flake = false;
 #    };
 
-    # use [flakes/xo-cmake],  but:
-    # - use nixpkgs established here
-    # - use xo_cmake source established here
-    xo_cmake = {
-      url = "./flakes/xo-cmake";
-      #inputs.nixpkgs.follows = "nixpkgs";
-      #inputs.xo_cmake_path.follows = "xo_cmake_path";
+    xo_cmake_path = {
+      type = "github";
+      owner = "Rconybea";
+      repo = "xo-cmake";
+      #ref = "v1.0";
+      #url = "https://github.com/Rconybea/xo-cmake";
+      #rev = "v1.0";
+      flake = false;
     };
+
+#    # use [flakes/xo-cmake],  but:
+#    # - use nixpkgs established here
+#    # - use xo_cmake source established here
+#    xo_cmake = {
+#      url = "./flakes/xo-cmake";
+#      #inputs.nixpkgs.follows = "nixpkgs";
+#      #inputs.xo_cmake_path.follows = "xo_cmake_path";
+#    };
 
 #    indentlog_flake = {
 #      url = "./flakes/indentlog";
@@ -28,7 +38,8 @@
   };
 
 #  outputs = { self, nixpkgs, xo_cmake_path, xo_cmake } :
-  outputs = { self, nixpkgs, xo_cmake } :
+#  outputs = { self, nixpkgs, xo_cmake } :
+  outputs = { self, nixpkgs, xo_cmake_path } :
     # self: directory of *this* flake in nix store
     # nixpkgs:  result of invoking nixpkgs flake on inputs.nixpkgs
     # xo_cmake: result of invoking xo_cmake flake on inputs.xo_cmake
@@ -39,7 +50,16 @@
     in {
       packages.${system} = {
         cowsay = pkgs.cowsay;
-        xo_cmake = xo_cmake.packages.${system}.xo_cmake;
+
+        xo_cmake = pkgs.stdenv.mkDerivation
+          {
+            name = "xo-cmake";
+            version = "1.0";
+            src = xo_cmake_path;
+            nativeBuildInputs = [ pkgs.cmake ];
+          };
+
+#        xo_cmake = xo_cmake.packages.${system}.xo_cmake;
 #        indentlog = indentlog_flake.packages.${system}.indentlog;
 
         ## importing non-flake nix package:
