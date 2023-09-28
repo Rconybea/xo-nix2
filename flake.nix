b6fade1fcc422dab764f7869c602e2c78af1406d
Roland Conybeare <rconybeare@gmail.com>
2023-09-28 14:44:37 -0400

    xo-nix2: working xo-cmake build with overrides

--- flake.nix
+++ flake.nix
@@ -7,14 +7,23 @@
     #nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/5d017a8822e0907fb96f7700a319f9fe2434de02.tar.gz";
     nixpkgs.url = "github:nixos/nixpkgs/23.05";
 
-    # use [flakes/xo-cmake],  but use nixpkgs established here
+    xo_cmake_path = {
+      type = "github"; owner = "Rconybea"; repo = "xo-cmake"; ref = "v1.0";
+      flake = false;
+    };
+
+    # use [flakes/xo-cmake],  but:
+    # - use nixpkgs established here
+    # - use xo_cmake source established here
     xo_cmake = {
-      url = "git+file:///home/roland/proj/xo-nix2?dir=flakes/xo-cmake";
+      url = "./flakes/xo-cmake";
+      #url = "git+file:///home/roland/proj/xo-nix2?dir=flakes/xo-cmake";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.xo_cmake_path.follows = "xo_cmake_path";
     };
   };
 
-  outputs = { self, nixpkgs, xo_cmake } :
+  outputs = { self, nixpkgs, xo_cmake_path, xo_cmake } :
     # self: directory of *this* flake in nix store
     # nixpkgs:  result of invoking nixpkgs flake on inputs.nixpkgs
     # xo_cmake: result of invoking xo_cmake flake on inputs.xo_cmake
--- flakes/xo-cmake/flake.nix
+++ flakes/xo-cmake/flake.nix
@@ -4,11 +4,21 @@
   # dependencies of this flake
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/23.05";
+
+    xo_cmake_path = {
+      type = "github";
+      owner = "Rconybea";
+      repo = "xo-cmake";
+      #ref = "v1.0";
+      #url = "https://github.com/Rconybea/xo-cmake";
+      #rev = "v1.0";
+      flake = false;
+    };
   };
 
   # function: inputs -> set
   #
-  outputs = { self, nixpkgs } :
+  outputs = { self, nixpkgs, xo_cmake_path } :
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
@@ -16,15 +26,18 @@
       packages.${system}.xo_cmake = pkgs.stdenv.mkDerivation
           {
             name = "xo-cmake";
-            # version = ...;
 
-            src = pkgs.fetchgit {
-              url = "https://github.com/rconybea/xo-cmake";
-              rev = "v1.0";
-              #rev = "v1.0";
-              #rev = "d349796363163419026d7ced0f46f3702ba4a2df";
-              #sha256 = "vak8TfHbHr/lHcZEsB6qjvoVM6xTdlu9EVbQbhJwPrE=";
-            };
+            version = "1.0";
+
+            src = xo_cmake_path;
+
+#            src = pkgs.fetchgit {
+#              #url = xo_cmake_path;
+#              #url = "github:Rconybea/xo-cmake";  # doesn't work
+#              #url = "https://github.com/Rconybea/xo-cmake";
+#              url = xo_cmake_path.url;
+#              rev = "v1.0";
+#            };
 
             nativeBuildInputs = with pkgs;
               [
@@ -32,6 +45,6 @@
               ];
           };
 
-      #defaultPackage.${system} = self.packages.${system}.xo_cmake;
+      defaultPackage.${system} = self.packages.${system}.xo_cmake;
     };
 }
