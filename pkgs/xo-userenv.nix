71c8fa59d27da412136ed4c268c02c7383dc452c
Roland Conybeare <rconybeare@gmail.com>
2024-03-13 21:27:00 -0400

    + missing deps for xo-userenv

--- pkgs/xo-userenv.nix
+++ pkgs/xo-userenv.nix
@@ -3,7 +3,10 @@
   buildFHSUserEnv, # ... other deps here
 
   # xo dependencies
-  xo-cmake, xo-indentlog
+  xo-cmake, xo-indentlog, xo-subsys, xo-refcnt, xo-randomgen, xo-ordinaltree, xo-pyutil, xo-reflect, xo-pyreflect,
+  xo-printjson, xo-pyprintjson, xo-callback, xo-webutil, xo-pywebutil, xo-reactor, xo-pyreactor, xo-simulator,
+  xo-distribution, xo-pydistribution, xo-process, xo-pyprocess, xo-statistics, xo-kalmanfilter,
+  xo-pykalmanfilter, xo-websock, xo-pywebsock,
 
   # other args
 
@@ -12,7 +15,33 @@
 
 buildFHSUserEnv {
   name = "xo-userenv";
-  targetPkgs = pkgs: [ xo-cmake xo-indentlog ];
+  targetPkgs = pkgs: [ xo-cmake
+                       xo-indentlog
+                       xo-subsys
+                       xo-refcnt
+                       xo-randomgen
+                       xo-ordinaltree
+                       xo-pyutil
+                       xo-reflect
+                       xo-pyreflect
+                       xo-printjson
+                       xo-pyprintjson
+                       xo-callback
+                       xo-webutil
+                       xo-pywebutil
+                       xo-reactor
+                       xo-pyreactor
+                       xo-simulator
+                       xo-distribution
+                       xo-pydistribution
+                       xo-process
+                       xo-pyprocess
+                       xo-statistics
+                       xo-kalmanfilter
+                       xo-pykalmanfilter
+                       xo-websock
+                       xo-pywebsock
+                     ];
   # runScript = ...;
   # profile = ...;
 }
