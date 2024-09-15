8bb10e5942ade8906760f04f19b1c087f0194f55
Roland Conybeare <rconybeare@gmail.com>
2024-09-15 13:28:36 -0500

    xo-nix2: XO_CMAKE_CONFIG_EXECUTABLE -> xo_pysimulator

--- pkgs/xo-pysimulator.nix
+++ pkgs/xo-pysimulator.nix
@@ -30,7 +30,8 @@ stdenv.mkDerivation (finalattrs:
       #rev = "c0472c9d7e4d2c53bfb977d3182380832fe96645";
     });
 
-    cmakeFlags = ["-DCMAKE_MODULE_PATH=${xo-cmake}/share/cmake"];
+    cmakeFlags = ["-DCMAKE_MODULE_PATH=${xo-cmake}/share/cmake"
+                  "-DXO_CMAKE_CONFIG_EXECUTABLE=${xo-cmake}/bin/xo-cmake-config"];
     doCheck = true;
     nativeBuildInputs = [ cmake catch2 xo-pyutil ];
     propagatedBuildInputs = [ xo-simulator xo-pyreactor ];
