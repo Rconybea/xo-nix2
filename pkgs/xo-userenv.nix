a9739d014cfefaeabc61f0e2fb6935b312cf4b41
Roland Conybeare <rconybeare@gmail.com>
2024-09-15 15:30:26 -0500

    xo-nix2: bugfix: fix xo-userenv

--- pkgs/xo-userenv.nix
+++ pkgs/xo-userenv.nix
@@ -3,10 +3,11 @@
   buildFHSUserEnv, # ... other deps here
 
   # xo dependencies
-  xo-cmake, xo-indentlog, xo-subsys, xo-refcnt, xo-randomgen, xo-ordinaltree, xo-pyutil, xo-reflect, xo-pyreflect,
+  xo-cmake, xo-indentlog, xo-subsys, xo-refcnt, xo-randomgen, xo-ordinaltree, xo-reflectutil,
+  xo-ratio, xo-pyutil, xo-reflect, xo-pyreflect,
   xo-printjson, xo-pyprintjson, xo-callback, xo-webutil, xo-pywebutil, xo-reactor, xo-pyreactor, xo-simulator,
   xo-pysimulator, xo-distribution, xo-pydistribution, xo-process, xo-pyprocess, xo-statistics, xo-kalmanfilter,
-  xo-pykalmanfilter, xo-websock, xo-pywebsock,
+  xo-pykalmanfilter, xo-websock, xo-pywebsock, xo-tokenizer, xo-expression, xo-pyexpression, xo-reader
 
   # other args
 
@@ -21,6 +22,8 @@ buildFHSUserEnv {
                        xo-refcnt
                        xo-randomgen
                        xo-ordinaltree
+                       xo-reflectutil
+                       xo-ratio
                        xo-pyutil
                        xo-reflect
                        xo-pyreflect
@@ -42,6 +45,10 @@ buildFHSUserEnv {
                        xo-pykalmanfilter
                        xo-websock
                        xo-pywebsock
+                       xo-tokenizer
+                       xo-expression
+                       xo-pyexpression
+                       xo-reader
                      ];
   # runScript = ...;
   # profile = ...;
